b7a5ff373c16a2a3d9e9b2d4c1e35b00f6ebfc06
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-12-30 01:27:08 -0500

    Fixed hero button styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,15 @@ const IndexPage = () => (
   <div className="indexpage">
     <Hero />
     <section className="intro">
-      <div className="intro__description">
-        Hi there! The name is Stephanie. I'm a Web Designer who also has extensive experience in graphic design, video editing and content marketing. I'm passionate about user experience design, community building and the psychological impact of product design.
-      </div>
       <img className="intro__profile-image" src={ ProfileImage } />
+      <div className="intro__wrapper">
+        <h1 className="intro__title">
+          About Stephanie
+        </h1>
+        <div className="intro__description">
+          Hi there! The name is Stephanie. I'm a Web Designer who also has extensive experience in graphic design, video editing and content marketing. I'm passionate about user experience design, community building and the psychological impact of product design.
+        </div>
+      </div>
     </section>
   </div>
 )
