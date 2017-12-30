e576295fbd3d5e641cfc404114513beb6fd0d46b
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-12-29 21:30:36 -0500

    Updated header: add gradient color to text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,30 @@ import Link from 'gatsby-link';
 
 import { slide as Menu } from 'react-burger-menu';
 
+
+const BurgerMenu = () => (
+  <Menu right className="menu">
+    <Link className="menu-item" to="/about/">About</Link>
+    <Link className="menu-item" to="/portfolio/">Portfolio</Link>
+    <Link className="menu-item" to="/contact/">Contact</Link>
+  </Menu>
+);
+
+const FullMenu = () => (
+  <nav className="header__navbar">
+      <Link className="item" to="/about/">About</Link>
+      <Link className="item" to="/portfolio/">Portfolio</Link>
+      <Link className="item" to="/contact/">Contact</Link>
+  </nav>
+);
+
 class Header extends Component {
 
   render(){
-
     return (
       <header className="header">
-          <Link className="header__logo" to="/">Impact! Media</Link>
-          <nav className="header__navbar">
-            <Menu right className="menu">
-              <Link className="menu-item" to="/about/">About</Link>
-              <Link className="menu-item" to="/portfolio/">Portfolio</Link>
-              <Link className="menu-item" to="/contact/">Contact</Link>
-            </Menu>
-          </nav>
+          <Link className="header__logo" to="/">[ <span>Impact!</span> media ]</Link>
+          <FullMenu />
       </header>
     );
   }
