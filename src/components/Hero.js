dca0247280e44ad25c1dec06cfb79b9b13427c0b
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-12-30 00:04:35 -0500

    Added hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,12 @@ import Link from 'gatsby-link'
 const Hero = () => (
   <div>
     <div className="hero">
-      <h1>CODE + IMPACT</h1>
-      <h2>Design is thinking made visible.</h2>
+      <h1 className="hero__title">CODE + IMPACT</h1>
+      <h2 className="hero__subtitle">Design is thinking made visible.</h2>
+      <div className="hero__cta">
+        <Link to="/about/" className="btn"><i className="fa fa-play-circle"></i>Watch Tour Video</Link>
+        <Link to="/contact/" className="btn"><i className="fa fa-envelope-o"></i>Get In Touch</Link>
+      </div>
     </div>
   </div>
 )
