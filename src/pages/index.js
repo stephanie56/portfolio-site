cbc8434e81225455c2742f7975ca1a967568f5c0
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-01-07 17:52:48 -0500

    Cleaned up section styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,23 @@ import Hero from '../components/Hero';
 
 import ProfileImage from '../images/profile-image.jpeg';
 
+const Intro = () => (
+  <section className="section">
+    <img className="profile-image" />
+    <h1 className="section__title">
+        Meet the Developer
+    </h1>
+    <h2 className="section__subtitle">Javascript + UX Design</h2>
+    <div className="section__description">
+        Stephanie is a Full Stack Front-end Developer specialised in making elegant websites and web apps that help you connect and engage with your customers.
+    </div>
+  </section>
+);
+
 const IndexPage = () => (
   <div className="indexpage">
     <Hero />
-    <section className="intro">
-      <img className="intro__profile-image" src={ ProfileImage } />
-      <div className="intro__wrapper">
-        <h1 className="intro__title">
-          About Stephanie
-        </h1>
-        <div className="intro__description">
-          Hi there! The name is Stephanie. I'm a Web Designer who also has extensive experience in graphic design, video editing and content marketing. I'm passionate about user experience design, community building and the psychological impact of product design.
-        </div>
-      </div>
-    </section>
+    <Intro />
   </div>
 )
 
